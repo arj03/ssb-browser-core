4db7eaf8c103d070acef3fd57e9a758c7b4ebe1d
Anders Rune Jensen <arj03@protonmail.ch>
2020-04-06 22:11:00 +0200

    Add disconnect method to tunnel message and minor tweaks

--- tunnel-message.js
+++ tunnel-message.js
@@ -14,25 +14,35 @@ exports.init = function (sbot, config) {
 
   var messages = Notify()
   var remotes = []
+  var incomingOk = ""
 
   sbot.on('rpc:connect', function (rpc, isClient) {
-    if (!isClient)
+    if (!isClient && rpc.id == incomingOk) {
+      incomingOk = ""
       remotes.push(rpc)
+
+      rpc.on('closed', (err) => {
+        remotes = remotes.filter(remote => remote.id != rpc.id)
+        messages({ type: "info", user: rpc.id, data: "disconnected" })
+      })
+    }
   })
-  
+
   return {
     acceptMessages: function(confirmHandler) {
       SSB.net.tunnel.setupIsConnectionOkHandler((remoteId) => {
         const isOk = confirmHandler(remoteId)
-        if (isOk)
-	  messages({ type: "info", user: remoteId, data: "connected" })
-	return isOk
+        if (isOk) {
+          messages({ type: "info", user: remoteId, data: "connected" })
+          incomingOk = remoteId
+        }
+        return isOk
       })
 
       SSB.net.connect(SSB.remoteAddress, (err, rpc) => {
-	if (err) throw(err)
+        if (err) throw(err)
 
-	rpc.tunnel.announce()
+        rpc.tunnel.announce()
       })
     },
     connect: function(remoteId) {
@@ -40,11 +50,25 @@ exports.init = function (sbot, config) {
       const remoteAddr = 'tunnel:@'+SSB.remoteAddress.split(':')[3]+ ':' + remoteId + '~shs:' + remoteKey
       messages({ type: "info", user: remoteId, data: "waiting for accept" })
       SSB.net.connect(remoteAddr, (err, rpc) => {
-	if (err) throw(err)
+        if (err) throw(err)
 
-	remotes.push(rpc)
-	messages({ type: "info", user: rpc.id, data: "connected" })
+        remotes.push(rpc)
+        messages({ type: "info", user: rpc.id, data: "connected" })
+
+        rpc.on('closed', (err) => {
+          remotes = remotes.filter(remote => remote.id != rpc.id)
+          messages({ type: "info", user: rpc.id, data: "disconnected" })
+        })
       })
+    },
+    disconnect: function() {
+      remotes.forEach(remote =>  {
+        try {
+          remote.close(true)
+        } catch (e) {}
+        messages({ type: "info", user: remote.id, data: "disconnected" })
+      })
+      remotes = []
     },
     sendMessage: function(type, message) {
       var toRemove = []
@@ -52,11 +76,13 @@ exports.init = function (sbot, config) {
         try {
           remote.tunnelMessage.message({ type, message })
         } catch (e) {
-	  messages({ type: "info", user: remote.id, data: "disconnected" })
+          messages({ type: "info", user: remote.id, data: "disconnected" })
           toRemove.push(remote)
         }
       })
-      remotes = remotes.filter(remote => !toRemove.includes(remote))
+      if (toRemove.length > 0)
+        remotes = remotes.filter(remote => !toRemove.includes(remote))
+
       messages({ type, user: SSB.net.id, data: message })
     },
     message: function(msg) {
