7b673d497b7468bb2d185c57cdef1f3dbaa500c3
Anders Rune Jensen <arj03@protonmail.ch>
2020-10-10 21:13:23 +0200

    minor ebt fix

--- ssb-db.js
+++ ssb-db.js
@@ -71,7 +71,7 @@ exports.init = function (sbot, config) {
 
   sbot.getAtSequence = function (seqid, cb) {
     // will NOT expose private plaintext
-    SSB.db.getClock(isString(seqid) ? seqid.split(':') : seqid, function (err, value) {
+    SSB.db.getClock((typeof seqid === 'string') ? seqid.split(':') : seqid, function (err, value) {
       if (err) cb(err)
       else cb(null, originalData(value))
     })
