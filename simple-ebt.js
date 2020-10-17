f205033eb0e2d61137eb546775f1d645871c8eab
Anders Rune Jensen <arj03@protonmail.ch>
2020-10-17 09:22:49 +0200

    better error handling in ebt

--- simple-ebt.js
+++ simple-ebt.js
@@ -102,7 +102,7 @@ exports.init = function (sbot, config) {
 
       for (var k in ebt.state.peers) {
         var peer = ebt.state.peers[k]
-        if(peer.clock[id] != null || peer.replicating[id] != null) {
+        if((peer.clock && peer.clock[id] != null) || (peer.replicating && peer.replicating[id] != null)) {
           var rep = peer.replicating && peer.replicating[id]
           data.peers[k] = {
             seq: peer.clock[id],
