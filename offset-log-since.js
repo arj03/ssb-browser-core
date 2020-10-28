bd4c7a982d64f4a5b42af29d017f1fc5ef07470d
Anders Rune Jensen <arj03@protonmail.ch>
2020-10-28 21:51:29 +0100

    Remove unused file

--- offset-log-since.js
+++
@@ -1,9 +0,0 @@
-// instead of flumelog-aligned-offset/compat which wraps stream in a
-// pull stream this only adds since
-const Obv = require('obv')
-
-module.exports = function(log) {
-  log.since = Obv()
-  log.onWrite = log.since.set
-  return log
-}
